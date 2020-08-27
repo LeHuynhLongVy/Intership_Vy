87d627da1220b85200af0cfdc45970b8e7fdf7b8
LeHuynhLongVy <vy.lehuynh21@gmail.com>
2020-08-27 10:54:41 +0700

    added routes

--- final_app/config/routes.rb
+++ final_app/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :photos
+  resources :albums
+  resources :users, except: [:index,:destroy]
+  namespace :admin do
+  resources :users
+  end
+
+  root to: "home#guestfeed"
+  get 'signup', to: 'home#signup'
+  get 'login', to: 'home#login'
+  get 'feed/photo', to: 'home#feed_photo'
+  get 'feed/album', to: 'home#feed_album'
+  get 'discover/photo', to: 'home#discover_photo'
+  get 'discover/album', to: 'home#discover_album'
 end
